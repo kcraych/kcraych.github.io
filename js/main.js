ec94b9c12e584bcfa24f4c90eaec365d30816e43
kcraych@gmail.com <kcraych@gmail.com>
2019-03-18 17:15:57 -0600

    Merge tablet and phone layout

--- js/main.js
+++ js/main.js
@@ -3,13 +3,6 @@ $(document).ready(() => {
     $("#nav-placeholder").load("nav.html");
 });
 
-
-
-function myFunction() {
-    var x = document.getElementById("my-menu-collapse");
-    if (x.className === "nav-menu-content") {
-        x.className += " responsive";
-    } else {
-        x.className = "nav-menu-content";
-    }
-}
+$(document).ready(() => {
+    $("#main-placeholder").load("home.html");
+});
