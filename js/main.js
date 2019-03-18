217ebf6bcdedbafcc7e82f184d61f98b4d40e247
kcraych@gmail.com <kcraych@gmail.com>
2019-03-18 17:41:17 -0600

    Add click listener to navbar

--- js/main.js
+++ js/main.js
@@ -6,3 +6,8 @@ $(document).ready(() => {
 $(document).ready(() => {
     $("#main-placeholder").load("home.html");
 });
+
+document.getElementByClassName("menu-item").addEventListener('click',function (){
+    var idClicked = jQuery(this).attr('id');
+    $("#main-placeholder").load(idClicked + ".html");
+} ); 
