38dce48e2ca9100167502489b460cc6aaf4e6c4f
kcraych <45825888+kcraych@users.noreply.github.com>
2019-03-19 13:08:44 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,10 +5,11 @@ $(document).ready(() => {
 $(document).ready(() => {
     $("#main-placeholder").load("home.html");
 });
-
+/*
 $(document).ready(() => {
     document.getElementByClassName("menu-item").on('click',function (){
         var idClicked = jQuery(this).attr('id');
         $("#main-placeholder").load(idClicked + ".html");
     }
 }); 
+*/
