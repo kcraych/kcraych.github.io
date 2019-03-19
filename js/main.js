39ed49e4533f888f1f0734e4807f3a5cfbe3fa68
kcraych <45825888+kcraych@users.noreply.github.com>
2019-03-19 13:06:32 -0600

    Add click listener

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 $(document).ready(() => {
     $("#nav-placeholder").load("nav.html");
 });
@@ -7,7 +6,9 @@ $(document).ready(() => {
     $("#main-placeholder").load("home.html");
 });
 
-document.getElementByClassName("menu-item").addEventListener('click',function (){
-    var idClicked = jQuery(this).attr('id');
-    $("#main-placeholder").load(idClicked + ".html");
-} ); 
+$(document).ready(() => {
+    document.getElementByClassName("menu-item").on('click',function (){
+        var idClicked = jQuery(this).attr('id');
+        $("#main-placeholder").load(idClicked + ".html");
+    }
+}); 
